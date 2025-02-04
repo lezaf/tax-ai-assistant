2a9ecf6cd6022dfd834d93ea442af737767a60ed
lezaf <25805070+lezaf@users.noreply.github.com>
2025-02-04 21:56:53 +0200

    Create a simple view to consume frontend API call

--- backend/advice/views.py
+++ backend/advice/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+@api_view(['POST'])
+def generate_advice(request):
+    '''
+    Creates an AI-generated advice based on user input.
+    '''
+    data = request.data
+
+    print("##################")
+    print(data)
+    print("##################")
+
+    return Response({'success': True,
+                     'detail': 'Form was submitted successfully!',
+                     'status': status.HTTP_200_OK})
