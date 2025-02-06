c8678102cb533ab5a8a7641926094a37ad03a158
lezaf <25805070+lezaf@users.noreply.github.com>
2025-02-06 21:03:23 +0200

    Fix Response status code

--- backend/advice/views.py
+++ backend/advice/views.py
@@ -15,9 +15,17 @@ def generate_advice(request):
     Creates an AI-generated advice based on user input.
     '''
     data = request.data
-    user_data = data['userData']
 
-    print(data)
+    # Check request has necessary fields
+    if 'userData' not in data:
+        return Response({'detail': 'userData field is missing.'},
+                        status=status.HTTP_400_BAD_REQUEST)
+    
+    if 'userPrompt' not in data:
+        return Response({'detail': 'userPrompt field is missing.'},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    user_data = data['userData']
 
     # Construct user's prompt
     user_data_str = ", ".join(f"{k}= {user_data[k]}" for k in user_data)
@@ -47,13 +55,13 @@ def generate_advice(request):
     except Exception as e:
         return Response({
             'success': False,
-            'detail': str(e),
-            'status': status.HTTP_500_INTERNAL_SERVER_ERROR
-        })
+            'detail': str(e)
+        },
+        status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     return Response({
         'success': True,
         'detail': 'Form was submitted successfully!',
-        'answer': chat_completion.choices[0].message.content,
-        'status': status.HTTP_200_OK
-        })
+        'answer': chat_completion.choices[0].message.content
+        },
+        status=status.HTTP_200_OK)
